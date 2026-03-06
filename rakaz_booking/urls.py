7291e80644a02fc3d6030fd01c30b9bdaa7cd645
almaghboub <almaghboub1112@gmail.com>
2026-03-06 23:19:28 +0800

    Redirect root URL to appointments page
    Add a redirect from the root URL to the appointments page.

--- rakaz_booking/urls.py
+++ rakaz_booking/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
@@ -12,5 +13,6 @@ urlpatterns += i18n_patterns(
     path('accounts/', include('django.contrib.auth.urls')),
     path('appointments/', include('appointments.urls')),
     path('dashboard/', include('dashboard.urls')),
+    path('', RedirectView.as_view(url='appointments/', permanent=False)),
     prefix_default_language=False,
 )
